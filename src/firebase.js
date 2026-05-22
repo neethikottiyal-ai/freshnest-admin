cc13939ab60195111ee2d41f75ad3f68d467288f
neethikottiyal-ai <neethikottiyal@gmail.com>
2026-05-22 13:18:01 +0530

    Update Firebase configuration for new project

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyDRrf6gYpMLt-vadzBcN9uyn706ipiPf8s",
-  authDomain: "freshnest-90da8.firebaseapp.com",
-  projectId: "freshnest-90da8",
-  storageBucket: "freshnest-90da8.appspot.com",
-  messagingSenderId: "805813095058",
-  appId:"1:805813095058:web:d8a4cf4da935b47c7db44b"
+  apiKey: "AIzaSyCuiSFExWTT4ku1xEWtLZkzJhDp2l38jMw",
+  authDomain: "freshnest-erp.firebaseapp.com",
+  projectId: "freshnest-erp",
+  storageBucket: "freshnest-erp.firebasestorage.app",
+  messagingSenderId: "378767473305",
+  appId: "1:378767473305:web:3fb10c5a71f801bd379608"
 };
 
 const app = initializeApp(firebaseConfig);
